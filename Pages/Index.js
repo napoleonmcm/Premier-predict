18cb2cb42ab12cefbd7a90804c9f33fbb84a5f56
napoleonmcm <158819567+napoleonmcm@users.noreply.github.com>
2026-01-21 12:54:19 +0100

    Add Home component for Premier Predict page

---
+++ Pages/Index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Premier Predict</h1>
+      <p>Predict football matches. Earn points.</p>
+      <a href="/register">Register</a> | <a href="/login">Login</a>
+    </main>
+  );
+}
